70ad0e9d204676b6d502f86dfa59dd5afb40801a
ChristianFfff <63169786+ChristianFfff@users.noreply.github.com>
2026-04-08 12:54:17 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -35,6 +35,8 @@ module.exports = function(config) {
 
   // pass some assets right through
   config.addPassthroughCopy("./src/site/images");
+  config.addPassthroughCopy("./src/site/*.png");
+  config.addPassthroughCopy("./src/site/*.webmanifest");
 
   // make the seed target act like prod
   env = (env=="seed") ? "prod" : env;
